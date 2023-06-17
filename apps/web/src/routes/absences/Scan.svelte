fef27fe57fe1139fbf5a06203a73588aa991280c
MonteBro <czech.illusion@gmail.com>
2023-06-17 10:12:10 +0200

    Implement Loading Overlay

--- apps/web/src/routes/absences/Scan.svelte
+++ apps/web/src/routes/absences/Scan.svelte
@@ -1,20 +1,26 @@
 <script lang="ts">
 	import CameraInput from './CameraInput.svelte';
+	import LoadingOverlay from './LoadingOverlay.svelte';
 
-	let takenImage:string;
-    let cameraInputRef;
+	let takenImage: string;
+	let cameraInputRef;
 
-    export const openCameraInput = ()=>{
-        cameraInputRef.openCamera()
-    }
+	export const openCameraInput = () => {
+		cameraInputRef.openCamera();
+	};
 
 	const handleCapture = (imgURL) => {
 		takenImage = imgURL;
 	};
+
+    let loadingText = "";
 </script>
 
-{#if takenImage}
-	<img src={takenImage} alt="Captured" class="mx-auto max-w-xs" />
-{:else}
-	<CameraInput on:capture={handleCapture} bind:this={cameraInputRef} />
-{/if}
+<main>
+	{#if takenImage}
+		<img src={takenImage} alt="Captured" class="mx-auto max-w-xs" />
+	{:else}
+		<CameraInput on:capture={handleCapture} bind:this={cameraInputRef} />
+	{/if}
+	<LoadingOverlay bind:message={loadingText}/>
+</main>

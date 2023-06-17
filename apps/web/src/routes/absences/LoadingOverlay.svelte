225f550bdc404b9bbc863ef94bb3c2a9b08679b5
MonteBro <czech.illusion@gmail.com>
2023-06-17 10:11:50 +0200

    Loading Overlay Component

---
+++ apps/web/src/routes/absences/LoadingOverlay.svelte
@@ -0,0 +1,16 @@
+<script>
+	import Stretch from '$lib/components/loading/Stretch.svelte';
+
+    export let message="Načítání ...";
+</script>
+
+<section
+	class="absolute top-0 left-0 h-full w-full flex justify-center items-center bg-base-900 bg-opacity-50"
+>
+	<div class="overlay-content flex flex-col justify-center items-center">
+		<Stretch size="50px" />
+        <span>
+            {message}
+        </span>
+	</div>
+</section>

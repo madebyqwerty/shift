f93c0508deb584812b9af7c0947d30045460b0b3
Tomáš Kebrle <dev@tomaskebrle.cz>
2023-08-20 12:09:58 +0200

    refactor: hide names

--- services/database/src/utils/seed.ts
+++ services/database/src/utils/seed.ts
@@ -1,38 +1,8 @@
 import { db } from "../kysely.ts";
+import { names } from "./names.ts";
 
 export async function seed() {
-  const users = [
-    "Auský Jan",
-    "Drbohlavová Lenka",
-    "Frolík Josef",
-    "Haunerová Magdaléna",
-    "Hausnerová Daniela",
-    "Horčička Vojtěch",
-    "Hořejší Tomáš",
-    "Jedličková Ellen",
-    "Kebrle Tomáš",
-    "Kernerová Eva",
-    "Krylová Linda",
-    "Kusá Adéla",
-    "Laubeová Kateřina",
-    "Matějková Emma Alžběta",
-    "Mytskanyuk Evelína",
-    "Parma Václav",
-    "Petrusová Magdaléna",
-    "Ransdorf Daniel",
-    "Sobotková Simona",
-    "Soukup Tadeáš",
-    "Srbek David",
-    "Srbek Matěj",
-    "Šedivá Mája",
-    "Thiebaut Marc",
-    "Trubačová Lucie",
-    "Vitmayerová Alexandra",
-    "Volek Petr",
-    "Witošová Eliška",
-    "Zeman Jan",
-    "Zouharová Magdaléna",
-  ].map((v) => ({
+  const users = names.map((v) => ({
     name: v,
   }));
 

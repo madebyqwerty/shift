4295cc1fe42abd7ba931de637cedb0649eef66e0
Tomáš Kebrle <dev@tomaskebrle.cz>
2023-08-20 12:09:58 +0200

    refactor: hide names

--- services/database/src/utils/seed.ts
+++ services/database/src/utils/seed.ts
@@ -1,27 +1,11 @@
 import { db } from "../kysely.ts";
+import { names } from "./names.ts";
 import { faker } from "../deps.ts";
 
 export async function seed() {
-  const users = Array(30)
-    .fill(undefined)
-    .map((v) => ({
-      name: faker.name.findName(),
-    }));
-
-  const dbUsers = await db
-    .insertInto("User")
-    .values(users)
-    .returningAll()
-    .execute();
-
-  dbUsers.map((user) => {
-    const absences = Array(10)
-      .fill(undefined)
-      .map((v) => ({
-        userId: user.id,
-        date: faker.date.between("2020-01-01", "2020-12-31"),
-        lesson: faker.random.number(10),
-      }));
+  const users = names.map((v) => ({
+    name: v,
+  }));
 
     db.insertInto("Absence").values(absences).execute();
   });

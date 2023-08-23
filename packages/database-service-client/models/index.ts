81c9e1d4b13c4b482ec82cc034064dcbb84f5631
tomaskebrle <tomaskebrle@users.noreply.github.com>
2023-08-23 07:19:32 +0000

    chore: update generated client [skip ci]

--- packages/database-service-client/models/index.ts
+++ packages/database-service-client/models/index.ts
@@ -1,8 +1,10 @@
 /* tslint:disable */
 /* eslint-disable */
 export * from './Absence';
-export * from './AbsencesUserIdPost200Response';
-export * from './AbsencesUserIdPostRequest';
+export * from './ApiAbsencesUserIdPost200Response';
+export * from './ApiAbsencesUserIdPostRequest';
+export * from './ApiUsersPostRequest';
 export * from './ModelError';
+export * from './Scan';
+export * from './ScanPost200Response';
 export * from './User';
-export * from './UsersPostRequest';

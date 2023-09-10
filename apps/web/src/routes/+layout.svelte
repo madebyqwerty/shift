a5d606b6e67dd21f92091bbe70ad7342da64f708
MonteBro <czech.illusion@gmail.com>
2023-09-10 17:19:58 +0200

    Delete svelte-device-info

--- apps/web/src/routes/+layout.svelte
+++ apps/web/src/routes/+layout.svelte
@@ -6,13 +6,21 @@
 	import Scans from '$lib/components/scans/Scans.svelte';
 	import { browser } from '$app/environment';
 	import { Focus } from 'lucide-svelte';
-	import { onMount } from 'svelte';
 	import { api } from '$lib/api/client';
 
-	let Device: any;
-
 	async function handleImageInput(event: InputEvent) {
-		console.log(event);
+		const file = event.target.files[0];
+		// const reader = new FileReader();
+		// reader.onload = (e) => {
+		// 	const blob = new Blob([e.target.result], { type: file.type });
+		// 	api.scanPostRaw({ img: blob, weekNumber: 4 }).then((res)=>{
+		//     console.log(res)
+		//   }).catch((err)=>{
+		//     console.log(err)
+		//   });
+		// };
+		// reader.readAsArrayBuffer(file);
+
 		const formData = new FormData();
 		formData.append('img', event.target.files[0]);
 		formData.append('week_number', '4');
@@ -23,53 +31,36 @@
 			console.log(res);
 		});
 	}
-
-	onMount(async () => {
-		try {
-		
-			const packageModule = await import('svelte-device-info');
-
-
-			Device = packageModule.default;
-
-		
-			console.log(Device);
-		} catch (error) {
-			console.error('Error importing npm package:', error);
-		}
-	});
 </script>
 
-{#if Device?.isPhone}
-	<div class="w-screen h-screen flex flex-col justify-center items-center">
-		<label for="img" class="cursor-pointer select-none p-[3vw] rounded-[4vw] bg-green-200">
-			<input
-				type="file"
-				name="img"
-				id="img"
-				on:input={handleImageInput}
-				accept="image/*"
-				class="hidden"
-				capture
-			/>
-			<Focus class="w-[25vw] h-[25vw] text-green-800" /></label
-		>
-	</div>
-{:else}
-	<div
-		class="w-full min-h-screen bg-background text-base-950 grid grid-cols-[255px_1fr] grid-rows-1 font-nunito-sans"
+<div class="w-screen h-screen hidden sm:flex flex-col justify-center items-center">
+	<label for="img" class="cursor-pointer select-none p-[3vw] rounded-[4vw] bg-green-200">
+		<input
+			type="file"
+			name="img"
+			id="img"
+			on:input={handleImageInput}
+			accept="image/*"
+			class="hidden"
+			capture
+		/>
+		<Focus class="w-[25vw] h-[25vw] text-green-800" /></label
 	>
-		<Toaster />
-		<Sidebar />
-		<main
-			class="p-6"
-			in:fly={{ x: 200, duration: 300, delay: 300 }}
-			out:fly={{ x: 200, duration: 300 }}
-		>
-			<slot />
-		</main>
-		{#if browser}
-			<Scans />
-		{/if}
-	</div>
-{/if}
+</div>
+
+<div
+	class="w-full min-h-screen bg-background text-base-950 grid grid-cols-[255px_1fr] grid-rows-1 font-nunito-sans sm:hidden"
+>
+	<Toaster />
+	<Sidebar />
+	<main
+		class="p-6"
+		in:fly={{ x: 200, duration: 300, delay: 300 }}
+		out:fly={{ x: 200, duration: 300 }}
+	>
+		<slot />
+	</main>
+	{#if browser}
+		<Scans />
+	{/if}
+</div>

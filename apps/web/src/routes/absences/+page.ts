2e1a65cb36af3d9408bcf887a4caeab71fec2f91
MonteBro <czech.illusion@gmail.com>
2023-09-12 22:39:14 +0200

    fetch absences

--- apps/web/src/routes/absences/+page.ts
+++ apps/web/src/routes/absences/+page.ts
@@ -1,5 +1,13 @@
 import type { PageLoad } from './$types';
+import { client } from '$lib/api/client';
 
 export const load = (async () => {
-	return {};
+	const usersArray = await client.apiUsersGet();
+	await Promise.all(
+		usersArray.map(async (user) => {
+			user['absences'] = await client.apiAbsencesUserIdGet({ userId: user.id });
+			return user;
+		})
+	);
+	return { students: usersArray };
 }) satisfies PageLoad;

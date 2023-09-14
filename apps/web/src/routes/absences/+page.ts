4d869c630fa4f7a53ec1daf99c032d3ece985175
Daniel <71315420+MonteDan@users.noreply.github.com>
2023-09-14 14:01:56 +0200

    User Array Type

--- apps/web/src/routes/absences/+page.ts
+++ apps/web/src/routes/absences/+page.ts
@@ -1,13 +1,19 @@
 import type { PageLoad } from './$types';
 import { client } from '$lib/api/client';
+import type { Absence, User } from '@shift/database-service-client';
+
+type UserWithAbsence = User & {absences?:Absence[]}
 
 export const load = (async () => {
-	const usersArray = await client.apiUsersGet();
+	const usersArray: UserWithAbsence[] = await client.apiUsersGet();
+
 	await Promise.all(
 		usersArray.map(async (user) => {
 			user['absences'] = await client.apiAbsencesUserIdGet({ userId: user.id });
 			return user;
 		})
-	);
+	
+	)
+
 	return { students: usersArray };
 }) satisfies PageLoad;

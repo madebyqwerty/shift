70d69e86fed1374fc9a6f7bb40a3545bff9d7db0
Daniel <71315420+MonteDan@users.noreply.github.com>
2023-09-14 14:24:24 +0200

    chore: fix types

--- apps/web/src/routes/absences/+page.ts
+++ apps/web/src/routes/absences/+page.ts
@@ -1,19 +1,16 @@
 import type { PageLoad } from './$types';
 import { client } from '$lib/api/client';
-import type { Absence, User } from '@shift/database-service-client';
-
-type UserWithAbsence = User & {absences?:Absence[]}
 
 export const load = (async () => {
-	const usersArray: UserWithAbsence[] = await client.apiUsersGet();
+	const usersArray = await client.apiUsersGet();
 
-	await Promise.all(
-		usersArray.map(async (user) => {
-			user['absences'] = await client.apiAbsencesUserIdGet({ userId: user.id });
-			return user;
-		})
+	const userArrayWithAbsence =await Promise.all(
+		usersArray.map(async (user) => ({
+			...user, absences: await client.apiAbsencesUserIdGet({ userId: user.id });
+			
+		}))
 	
 	)
 
-	return { students: usersArray };
+	return { students: userArrayWithAbsence };
 }) satisfies PageLoad;
